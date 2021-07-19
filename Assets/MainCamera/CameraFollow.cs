0e19521eb72974646a8fb85c3662e7f95bf27f19
mustafacomert <mustafacomert00@gmail.com>
2021-07-19 21:44:38 +0300

    Running Forward

---
+++ Assets/MainCamera/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform targetBoy;
+    private Vector3 desiredPos;
+    private float offsetZ;
+   
+    private void Awake()
+    {
+        offsetZ = 11f;
+        desiredPos = transform.position;
+        targetBoy = GameObject.FindGameObjectWithTag("Boy").transform;
+       
+    }
+    private void LateUpdate()
+    {
+        desiredPos.z = targetBoy.position.z - offsetZ;
+        transform.position = desiredPos; 
+    }
+}
---
+++ Assets/TestProjectAssets/PlayerCharacter/BoyController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoyController : MonoBehaviour
+{
+    [SerializeField] private float forwardSpeed = 5f;
+    private Rigidbody rb;
+    private float targetXPos;
+    private float horizontalSpeed = 5f;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(new Vector3(Mathf.Lerp(transform.position.x, targetXPos, horizontalSpeed*Time.fixedDeltaTime), 
+                        rb.velocity.y, 
+                        transform.position.z + forwardSpeed * Time.fixedDeltaTime));
+    }
+}
