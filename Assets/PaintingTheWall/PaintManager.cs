78e1d5fee4ad87ff993a3ccadfa17a0166c5344f
mustafacomert <mustafacomert00@gmail.com>
2021-07-25 01:01:53 +0300

    Paint the wall using physics.raycast

--- Assets/PaintingTheWall/PaintManager.cs
+++ Assets/PaintingTheWall/PaintManager.cs
@@ -3,35 +3,25 @@
 public class PaintManager : MonoBehaviour
 {
     [SerializeField] private GameObject brushPrefab;
-    [SerializeField] private GameObject wall;
-    private Plane plane;
-    private GameObject trail;
-    private float distance;
-
+    private Ray ray;
+    private Camera mainCamera;
     private void Awake()
     {
-        MeshFilter filter = wall.GetComponent<MeshFilter>();
-        Vector3 normal = new Vector3();
-
-        if (filter && filter.mesh.normals.Length > 0)
-            normal = filter.transform.TransformDirection(filter.mesh.normals[0]);
-
-        plane = new Plane(normal.normalized, wall.transform.position);
+        mainCamera = Camera.main;    
     }
+
     private void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButton(0))
         {
-            
-        }
-        else if(Input.GetMouseButton(0))
-        {
-            //trail = (GameObject)Instantiate(brushPrefab, Input.mousePosition, Quaternion.identity);
-            trail = (GameObject)Instantiate(brushPrefab, Input.mousePosition, Quaternion.identity);
-            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (plane.Raycast(mouseRay, out distance))
+            ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
             {
-                trail.transform.position = mouseRay.GetPoint(distance);
+                if(hit.collider.CompareTag("Wall"))
+                {
+                    Instantiate(brushPrefab, hit.point + Vector3.forward * -0.1f, Quaternion.identity, transform);
+                }
             }
         }
     }
