6bd89965f304b9507e44ee42c93a1ab24bcd48cf
mustafacomert <mustafacomert00@gmail.com>
2021-07-25 14:14:23 +0300

    paint spilling out of the wall issue solved.

--- Assets/PaintingTheWall/PaintManager.cs
+++ Assets/PaintingTheWall/PaintManager.cs
@@ -5,6 +5,11 @@ public class PaintManager : MonoBehaviour
     [SerializeField] private GameObject brushPrefab;
     private Ray ray;
     private Camera mainCamera;
+    private float minXValue = -6f;
+    private float minYValue = 0.5f;
+    private float maxYValue = 9.5f;
+    private float clampedXPos;
+    private float clampedYPos;
     private void Awake()
     {
         mainCamera = Camera.main;    
@@ -16,11 +21,15 @@ public class PaintManager : MonoBehaviour
         {
             ray = mainCamera.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
+            Quaternion rotation = Quaternion.Euler(-90, 0, 0);
             if(Physics.Raycast(ray, out hit))
             {
                 if(hit.collider.CompareTag("Wall"))
                 {
-                    Instantiate(brushPrefab, hit.point + Vector3.forward * -0.1f, Quaternion.identity, transform);
+                    clampedXPos = Mathf.Clamp(hit.point.x, minXValue, -minXValue);
+                    clampedYPos = Mathf.Clamp(hit.point.y, minYValue, maxYValue);
+                    hit.point = new Vector3(clampedXPos, clampedYPos, hit.point.z);
+                    Instantiate(brushPrefab, hit.point + Vector3.forward * -0.1f, rotation, transform);
                 }
             }
         }
