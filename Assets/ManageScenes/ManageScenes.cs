dd440dd91b2c827e95592df7039ddf048695c5da
mustafacomert <mustafacomert00@gmail.com>
2021-07-28 21:38:15 +0300

    beginner level and menu added

---
+++ Assets/ManageScenes/ManageScenes.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManageScenes : MonoBehaviour
+{
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    public void LoadBeginnerLevel()
+    {
+        SceneManager.LoadScene("BeginnerLevel"); 
+    }
+
+    public void LoadExpertLevel()
+    {
+        SceneManager.LoadScene("ExpertLevel");
+    }
+
+}
