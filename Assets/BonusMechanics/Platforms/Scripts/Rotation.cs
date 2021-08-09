11280e055c8704435c21288d06f9f305f327199b
mustafacomert <mustafacomert00@gmail.com>
2021-08-10 02:41:30 +0300

    Organize Folders

---
+++ Assets/BonusMechanics/Platforms/Scripts/Rotation.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Rotation : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 40f;
+    private enum Directions
+    {
+        left, right
+    }
+    
+    [SerializeField] private Directions dir;
+    private HashSet<Rigidbody> pickedUpObjs;
+    private HashSet<GameObject> pickedUpBefore;
+    private void Awake()
+    {
+        if(dir == Directions.left)
+        {
+            rotationSpeed = -rotationSpeed;
+        }
+        pickedUpObjs = new HashSet<Rigidbody>();
+        pickedUpBefore = new HashSet<GameObject>();
+    }
+    private void Update()
+    {
+        transform.Rotate(Vector3.back * Time.deltaTime * rotationSpeed);       
+    }
+
+    private void FixedUpdate()
+    {
+        foreach (var t in pickedUpObjs)
+        {
+            GameObject go = t.gameObject;
+            if (go.CompareTag("Girl") && !pickedUpBefore.Contains(go))
+            {
+                pickedUpBefore.Add(go);
+                Debug.Log("ADASDDDDDAAD");
+                OpponentController opponent = go.GetComponent<OpponentController>();
+                NavMeshAgent nav = opponent.navMeshAgent;
+                Rigidbody rb = go.GetComponent<Rigidbody>();
+                Vector3 dest = opponent.finishLine.position;
+                nav.enabled = false;
+                rb.isKinematic = false;
+
+                StartCoroutine(ApplyForceAndReenableNavAgent(nav, rb, dest));
+            }
+            else if(t.gameObject.CompareTag("Boy"))
+            {
+                //t.position += Vector3.right * 4f * Time.deltaTime;
+                t.AddForce(Vector3.right * (8 * rotationSpeed) * Time.fixedDeltaTime, ForceMode.VelocityChange);
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Collider c = collision.collider;
+        if (c.CompareTag("Boy") || c.CompareTag("Girl"))
+        {
+            pickedUpObjs.Add(c.attachedRigidbody);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        Collider c = collision.collider;
+        if (c.CompareTag("Boy") || c.CompareTag("Girl"))
+        {
+            pickedUpObjs.Remove(c.gameObject.GetComponent<Rigidbody>());
+        }
+    }
+
+    private IEnumerator ApplyForceAndReenableNavAgent(NavMeshAgent nav, Rigidbody rb, Vector3 destination)
+    {
+        rb.AddForce(Vector3.right * (rotationSpeed*8) * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        yield return new WaitForSeconds(0.5f);
+        rb.isKinematic = true;
+        nav.enabled = true;
+        nav.SetDestination(destination);
+    }
+
+}
---
+++ Assets/OpponentCharacter/Scripts/OpponentController.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class OpponentController : MonoBehaviour
+{
+    //GameObjects from scene
+    //
+    //Destination of the navMeshAgent
+    public Transform finishLine { get; private set; }
+    private Transform spawnPoint;
+    private TextMeshProUGUI txt;
+    private Transform boy;
+    //Component of boy object
+    private BoyController boyController;
+    //Component of that object
+    public NavMeshAgent navMeshAgent { get; private set; }
+    private Rigidbody rb;
+    //state variable
+    private bool isBehind;
+    private int opponentCount;
+    private static int rank;
+
+    private void Awake()
+    {
+        //Init GameObjects from scene
+        finishLine = GameObject.FindGameObjectWithTag("FinishLine").transform;
+        spawnPoint = GameObject.FindGameObjectWithTag("SpawnPoint").transform;
+        txt = GameObject.FindGameObjectWithTag("RankingBoard").GetComponent<TextMeshProUGUI>();
+        boy = GameObject.FindGameObjectWithTag("Boy").transform;
+        boyController = boy.gameObject.GetComponent<BoyController>();
+
+        //how many oppenents are there 
+        opponentCount = transform.parent.childCount;
+        rank = opponentCount + 1;
+        //Init ranking board text
+        txt.text = rank.ToString() + " / " + rank.ToString();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        rb = GetComponent<Rigidbody>();
+        //calculate random destination along the x-axis on finish line
+        var x = Random.Range(0, 10000);
+        if(x % 2 == 0)
+        {
+            x = x % 6;
+        }
+        else
+        {
+            x = (x % 6) * -1;
+        }
+        Vector3 dest = new Vector3(x, finishLine.position.y, finishLine.position.z);
+        navMeshAgent.SetDestination(dest);
+        //navMeshAgent.updatePosition = false;
+    }
+
+    private void Update()
+    {
+        //change the ranking of the boy
+        if (!boyController.raceFinished)
+        {
+            
+            if (isBehind)
+            {
+                //if oppenent is behind, check if this object pass the boy or not
+                if (boy.position.z < transform.position.z)
+                {
+                    //if it passed, increase the rank of the boy and change the ranking board w.r.t that fact
+                    isBehind = false;
+                    ++rank;
+                    txt.text = rank.ToString() + " / " + (opponentCount + 1).ToString();
+                }
+            }
+            else
+            {
+                //if oppenent is front of the boy, check if boy pass this object or not
+                if (boy.position.z > transform.position.z)
+                {
+                    //if boy passed, decrease the rank of the boy and change the ranking board w.r.t that fact
+                    isBehind = true;
+                    --rank;
+                    txt.text = rank.ToString() + " / " + (opponentCount + 1).ToString();
+                }
+            }
+        }
+        //if this oppenent pass the finish line, destroy it
+        if(finishLine.position.z - transform.position.z < 0.01f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //if opponent hits a obtacle, send her to the starting position
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+            navMeshAgent.enabled = false;
+            transform.position = spawnPoint.position;
+            navMeshAgent.enabled = true;
+            var x = Random.Range(-6.5f, 6.5f);
+            Vector3 dest = new Vector3(x, finishLine.position.y, finishLine.position.z);
+            navMeshAgent.SetDestination(dest);
+        }
+    }
+   
+    //below three function used for setting, static variable, on scene load
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+    }
+
+    void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        rank = opponentCount + 1;
+    }
+
+}
